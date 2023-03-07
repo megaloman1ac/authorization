db9ec3cea84946eb3e9821ea2e1b80f853ce734a
megaloman1ac <crystalshow44@gmail.com>
2023-03-07 21:47:30 +0300

    'SQL' module for requests

--- modules/sql.py
+++ modules/sql.py
@@ -5,11 +5,17 @@ class SQL:
 	# At first, i've created simple "select" query function 
 	def sql_query_select(login, password):
 		
+		
+		# Connection
 		connection = sqlite3.connect("users.db")
 		cur = connection.cursor()
-				
+		
+		# Creating and executing a command	
 		command = "select * from persons where login = '{0}' and pass = '{1}'".format(login, password)
 		cur.execute(command)
+		
+		
+		# Checking for users (if it has row)
 		
 		if len(cur.fetchall()) > 0:
 			return 1
@@ -19,13 +25,18 @@ class SQL:
 	# At first, i've created simpole "insert" query function
 	def sql_query_insert(login, password):
 		try:
+			# Connection
 			connection = sqlite3.connect("users.db")
 			cur = connection.cursor()
-		
+			
+			# Creating and executing a command	
 			command = "insert into persons values ('{0}', '{1}')".format(login, password)
 			cur.execute(command)
-		
+			
+			# Add user to DB
 			connection.commit()
+			
+			# Return 1 if it OK
 			return 1
 		except ValueError:
 			return 0
